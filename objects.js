3f6247ba09d9eeda3fc60387391848631454c3fa
Emily Venturino <ebventurino@gmail.com>
2018-05-29 16:50:48 -0500

    added challenge

--- objects.js
+++ objects.js
@@ -7,40 +7,74 @@ const sanger = {
         "jobs": "I believe in jobs",
         "infrastructure": true,
         "healthCare": true,
-        "crimeEnforcement": false 
+        "crimeEnforcement": false
     },
 
     urldonationForm: { website: "www.site.com" },
     calendarOfEvents: "calendar",
     volunteerInfo: {
-            "name": "Bob Smith",
-            "address": "1234 address ln",
-            "email": "email@website.com",
-            "phoneNumber": "234-234-2300",
-            "availability": true,
-            "activities": "polls",
-        },
+        "name": "Bob Smith",
+        "address": "1234 address ln",
+        "email": "email@website.com",
+        "phoneNumber": "234-234-2300",
+        "availability": true,
+        "activities": "polls",
+    },
     biography: "this is a bio",
     imageGallery: {
-            "HeadShot": "photo",
-            "Picture of family":"photo",
-            "Picture of consttuents": "photo",
+        "headShot": "photo",
+        "familyPhoto": "photo",
+        "Picture of consttuents": "photo",
     },
     missionStatement: "This is my mission statement",
     urlForRegistering: "www.url.com"
 }
 
-function changeDistrict (anyDistrict) {
+
+
+
+function changeDistrict(anyDistrict) {
     sanger.district = anyDistrict
     console.log(sanger.district)
 }
 changeDistrict("4th")
 
-function changeVolInfo (myObject,myProperty) {
- myObject = "userInfo"
- myProperty = "Bobby Smith"
- console.log(sanger.volunteerInfo)
- console.log(volunteerInfo.name)
+function changeVolInfo(newEmail,newName) {
+    sanger.volunteerInfo.email = newEmail;
+    sanger.volunteerInfo.name = newName
+    console.log(sanger.volunteerInfo)
 }
+changeVolInfo("New Email", "Bobby Smith");
 
-changeVolInfo(sanger.volunteerInfo),volunteerInfo.name;
+function changePlatform(healthCareProperty,taxesProperty) {
+    sanger.platformStatements.healthCare = healthCareProperty
+    sanger.platformStatements.taxes = taxesProperty
+    console.log(sanger.platformStatements)
+}
+changePlatform(false, false)
+
+function changeImageGallery(headshotProperty,familyPhotoProperty) {
+  sanger.imageGallery.headShot = headshotProperty;
+  sanger.imageGallery.familyPhoto = familyPhotoProperty;
+  console.log(sanger.imageGallery)
+}
+changeImageGallery ("New Headshot Photo", "new Family Photo")
+
+function changeMission (changeMissionStatement) {
+    sanger.missionStatement = changeMissionStatement;
+    console.log(changeMissionStatement)
+} 
+changeMission("This is a new mission statement")
+
+function newBio (changeBio) {
+    sanger.biography = changeBio;
+    console.log(changeBio)
+}
+newBio("This is a new bio")
+
+
+let newArticle = document.createElement("article")
+let newNode = document.createTextNode("This is a new article")
+
+newArticle.appendChild(newNode)
+console.log(newNode)
